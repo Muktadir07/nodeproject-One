2facb50a989d8b37443dda5617c7a543a0d948e6
Muktadir07 <muktadir.ahmmad@gmail.com>
2023-03-12 02:16:36 +0600

    nodemoon installed

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('hello from my app');
 }); 
-app.listen(3000,()=>{
-    console.log(`listening on port ${chalk.green('3000')}`);
+app.listen(PORT,()=>{
+    debug(`listening on port ${chalk.green(PORT)}`);
 });
